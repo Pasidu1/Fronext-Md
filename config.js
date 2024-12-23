34d1ebf30d4fb9584e17d918675b3fc0d7e85aec
Pasidu1 <163862208+Pasidu1@users.noreply.github.com>
2024-12-23 16:13:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: procesess.env.ALIVE_IMG || "https://i.ibb.co/hY5RLdr/7471.jpg" 
-ALIVE_MSG: procesess.env.ALIVE_IMG || "Hello am cyber froly MMatrix team member I am alive now"
+ALIVE_MSG: procesess.env.ALIVE_IMG || "Hello am cyber froly Matrix team member I am alive now"
 };
