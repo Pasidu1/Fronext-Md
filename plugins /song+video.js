d3e237c2f6ee751da5e19c5df3e3e93189f378f1
Pasidu1 <163862208+Pasidu1@users.noreply.github.com>
2024-12-25 18:48:59 +0530

    Update song+video.js

--- plugins /song+video.js
+++ plugins /song+video.js
@@ -5,7 +5,7 @@ const yts = require(`yt-search`)
 
 cmd({
     pattern: "song",
-    desc: "download.",
+    desc: "download song",
     category: "download",
     filename: __filename
 },
@@ -34,9 +34,56 @@ await conn.sendMassage(frome,{imge:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downloadUrl = down.dl_urlurl
 
-//send audio message
+//send audio + document message
 await conn.sendMessage(from,{audio: {url}:downloadUrl,mimetype:"audio/mpeg"},{quoted:mek}) 
-  
+await conn.sendMessage(from,{document: {url}:downloadUrl,mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*MADE BY CYBER FROLY*"},{quoted:mek})  
+
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+{
+})
+
+//============video-dl============
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please give me url or title❓*")  
+const search = awit yts(q)  
+const data = search.videos[0]:
+const urlurl
+
+let desc =`
+🌟*FRONEXT MD VIDEO DOWNLOAD*🌟
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+*MADE BY CYBER FROLY 🧡*
+`  
+await conn.sendMassage(frome,{imge:{url: data.thumbnail},caption:desc},{quoted:mek});  
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_urlurl
+
+//send video+ document message
+await conn.sendMessage(from,{video: {url}:downloadUrl,mimetype:"video/mp4",caption:"*MADE BY CYBER FROLY🧡*"},{quoted:mek}) 
+await conn.sendMessage(from,{document: {url}:downloadUrl,mimetype:"video/mp4",fileName:data.title + ".mp4"},{quoted:mek}) 
+    
 
 
 
@@ -47,3 +94,6 @@ console.log(e)
 reply(`${e}`)
 {
 })  
+
+
+
