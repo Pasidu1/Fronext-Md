9007dbd7e8ef3c99dc60539ecceebad0f88a6d4e
Lakshan Damayantha <damayanthalakshan8@gmail.com>
2024-12-30 15:32:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "TMkkBbaL#BKs1m4kXfJI_hsgYPIUDJvasNZYpfVxGvVVZol6DWxI",
 ALIVE_IMG: procesess.env.ALIVE_IMG || "https://i.ibb.co/hY5RLdr/7471.jpg" 
 ALIVE_MSG: procesess.env.ALIVE_IMG || "Hello am cyber froly Matrix team member I am alive now👋",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",    
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"   
 };
