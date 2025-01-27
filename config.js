a46431d498b36d3d546b63b6161b4e4b8050fb6b
Nadeenpoornalk <dinkafilms02@gmail.com>
2025-01-27 22:53:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2j5ESKoB#MJZ7fhSZOMKCC9b3WRqq94f4gljxebg6723oaY281BI",
+SESSION_ID: process.env.SESSION_ID || "2j5ESKoB#MJZ7fhSZOMKCC9b3WRqq94f4gljxebg6723oaY281BI",
 ALIVE_IMG: procesess.env.ALIVE_IMG || "https://i.ibb.co/hY5RLdr/7471.jpg" 
 ALIVE_MSG: procesess.env.ALIVE_IMG || "Hello am cyber froly Matrix team member I am alive now👋",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"   
