280293452fe5bf86aceeb262ff63a5fe764e2b2a
Nadeenpoorna-lkz <nadeepoorna987@gmail.com>
2025-02-03 23:50:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: procesess.env.ALIVE_IMG || "https://i.ibb.co/hY5RLdr/7471.jpg" 
-ALIVE_MSG: procesess.env.ALIVE_IMG || "Hello am cyber froly Matrix team member I am alive now👋",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"   
+ALIVE_IMG: procesess.env.ALIVE_IMG || "https://i.ibb.co/hY5RLdr/7471.jpg",
+ALIVE_MSG: procesess.env.ALIVE_MSG|| "Hello am cyber froly Matrix team member I am alive now👋"
 };
